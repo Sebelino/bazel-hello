07a0170ccc91b5ff7d4b0ee9b5c146e023608bcf
Sebastian Olsson <sebelino7@gmail.com>
2020-11-02 14:28:28 +0100

    Decrease sleep

--- src/main/java/se/tink/hello/HelloWorld.java
+++ src/main/java/se/tink/hello/HelloWorld.java
@@ -3,7 +3,7 @@ package se.tink.hello;
 public class HelloWorld {
 
     public static void main(String[] args) throws InterruptedException {
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 3; i++) {
             System.out.println("hoyworld");
             Thread.sleep(1000);
         }
